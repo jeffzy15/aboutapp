1e78aa4d9e28a0dbdc4dbf9415c3bdda5fcbd1c9
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-05-31 14:54:30 +0800

    New Update

--- about/P2View.swift
+++ about/P2View.swift
@@ -1,15 +1,92 @@
-//
-//  P2View.swift
-//  about
-//
-//  Created by Allen Jeffrey on 31/5/23.
-//
-
 import SwiftUI
+import AVFoundation
 
 struct P2View: View {
+    @State private var index = 0
+    @State private var player: AVAudioPlayer?
+    @State private var makespring = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.black]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Click for special effect ->")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .bold()
+                        .font(.title2)
+                        .cornerRadius(10)
+                    Button(action: {
+                        if let url = URL(string: "https://tk.sg/ImportantLink") {
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                            withAnimation {
+                                index += 100
+                                playSoundtrack()
+                            }
+                        }
+                    }, label: {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                    })
+                }
+                .padding(.top, 20)
+                if makespring {
+                    Image("lmao")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .scaleEffect(CGFloat(index) / 100.0 + 1.0)
+                        .animation(.spring())
+                }
+                Text("Ethan")
+                    .padding()
+                    .frame(maxWidth: 150)
+                    .foregroundColor(.white)
+                    .bold()
+                    .underline()
+                    .font(.largeTitle)
+                    .cornerRadius(10)
+                
+                Text("About:\n- Studies at SJI\n- Enjoys Coding and Gaming")
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.title)
+                    .cornerRadius(10)
+            }
+        }
+        .onAppear {
+            prepareSoundtrack()
+        }
+    }
+    
+    func prepareSoundtrack() {
+        guard let url = Bundle.main.url(forResource: "hehe", withExtension: "mp3") else {
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.prepareToPlay()
+        } catch {
+            print("Failed to load soundtrack.")
+        }
+    }
+    
+    func playSoundtrack() {
+        player?.currentTime = 0
+        player?.play()
     }
 }
 
