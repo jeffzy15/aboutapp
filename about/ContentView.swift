b7ff62c6fa15420525bd3f94f455f3b124fd134e
Allen Jeffrey <mpjallenjeffrey@gmail.com>
2023-05-31 13:42:36 +0800

    Initial Commit

---
+++ about/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  about
+//
+//  Created by Allen Jeffrey on 31/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ about/aboutApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutApp.swift
+//  about
+//
+//  Created by Allen Jeffrey on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
