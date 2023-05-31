b22950a441754b891ec26756ba8f706efdf9b965
Allen Jeffrey <mpjallenjeffrey@gmail.com>
2023-05-31 15:19:26 +0800

    add Jeffrey's view

--- about/P1View.swift
+++ about/P1View.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct P1View: View {
+    @State private var score = 0
+    @State private var sheet = false
     var body: some View {
-        Text("Test")
+        ZStack{
+            LinearGradient(colors: [.blue, .white], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            HStack{
+                Image("jeffrey")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                VStack{
+                    Button {
+                        score += 1
+                        if score > 5{
+                            sheet = true
+                        }
+                    } label: {
+                        Text("Allen Jeffrey")
+                            .padding()
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .background(.black)
+                            .cornerRadius(10)
+                    }
+                    
+                    Text("I like to code")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                }
+            }
+        }
+        .sheet(isPresented: $sheet){
+            Text("Why did you click?")
+        } .presentationDetents([.medium])
     }
 }
 
